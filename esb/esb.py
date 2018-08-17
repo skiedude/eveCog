4df9936decf75959cc0cf796b0f46eb5a5f4e946
Jake Z <skibumb@gmail.com>
2018-08-17 13:14:28 -0600

    fix url, and help

--- esb/esb.py
+++ esb/esb.py
@@ -11,14 +11,16 @@ class esb:
     @commands.group(pass_context=True)
     async def esb(self, ctx):
         """Eveskillboard related commands"""
-        
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+            
     @esb.command()
     async def pilot(self, *, pilot_name: str):
         """Look up a pilots skillboard"""
 
         pilot_found = requests.get(self.esb_url + pilot_name)
         if pilot_found.status_code == 200:
-            await self.bot.say("Pilot Found! \n" + esb_url + pilot_name)
+            await self.bot.say("Pilot Found! \n" + self.esb_url + pilot_name)
         else:
             await self.bot.say("Pilot wasn't found on Eveskillboard")
     
