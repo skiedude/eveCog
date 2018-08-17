b25290fee46c90f79cbc4209c563b64f9e31ca2e
Jake Z <skibumb@gmail.com>
2018-08-16 18:58:25 -0600

    more update

--- evestatus/evestatus.py
+++ evestatus/evestatus.py
@@ -11,13 +11,12 @@ class evestatus:
 
     @commands.command()
     async def evestats(self):
-        """Returns the Current time, players online, start_time, vip status in EVE Online"""
+        """Returns the Current time, players online in EVE Online"""
 
-        utc_time = datetime.utc_time().strftime('%I:%M:%S %p')
-        try:
-        	current_status = requests.get(eve_url + '/v1/status/')
-        	current_status.json()
-        await self.bot.say("Current Time: " + utc_time + "\n")
+        utc_time = datetime.utcnow().strftime('%I:%M:%S %p')
+        current_status = requests.get(eve_url + '/v1/status/')
+        rsp = current_status.json()
+        await self.bot.say("Current Time: " + utc_time + "\n Player Count: " + rsp['players'])
 
 def setup(bot):
    	bot.add_cog(evestatus(bot))
