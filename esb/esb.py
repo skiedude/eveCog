402917d0ced3b51188bbd03e415ad44608d04d10
Jake Z <skibumb@gmail.com>
2018-08-17 13:10:16 -0600

    Initial commit of esb cog, to query pilots on eveskillboard

---
+++ esb/esb.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+from discord.ext import commands
+
+class esb:
+    esb_url = "https://eveskillboard.com/pilot/"
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(pass_context=True)
+    async def esb(self, ctx):
+        """Eveskillboard related commands"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("Unknown command passed to esb")
+
+    @esb.command()
+    async def pilot(self, *, pilot_name: str):
+        """Look up a pilots skillboard"""
+
+        pilot_found = requests.get(self.esb_url + pilot_name)
+        if pilot_found.status_code == 200:
+            await self.bot.say("Pilot Found! \n" + esb_url + pilot_name)
+        else:
+            await self.bot.say("Pilot wasn't found on Eveskillboard")
+    
+def setup(bot):
+   	bot.add_cog(esb(bot))
