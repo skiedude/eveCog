1c3eed74350c8a4f67cc242a0ac217dc44c138b0
Jake Z <skibumb@gmail.com>
2019-05-04 21:05:01 +0000

    naming, removing some unused things

--- evedead/evedead.py
+++ evedead/evedead.py
@@ -1,11 +1,9 @@
 import discord
-import requests
 import random
 from discord.ext import commands
 from datetime import datetime
 
-class evedead:
-    eve_url = "https://esi.evetech.net"
+class eveisdead:
 
     def __init__(self, bot):
         self.bot = bot
@@ -17,4 +15,4 @@ class evedead:
         await self.bot.say(random.choice(answers))
 
 def setup(bot):
-   	bot.add_cog(evedead(bot))
+   	bot.add_cog(eveisdead(bot))
